4f4cafa4ddf89a11cc0dcdc6119ba6433b76bfbc
kamalakars04 <kamalakars04@gmail.com>
2020-11-18 20:21:23 +0530

    [Kamalakar].[Refactor].Adding UC4_AdditionalRegexCondition

--- PincodeRegex.js
+++ PincodeRegex.js
@@ -1,7 +1,8 @@
 // UC 1 Write regular expression to validate pin of six digit
 // UC 2 Dont allow any character at the beginning 
 // UC 3 Dont allow any character at the end
-let pincodeRegex = RegExp("^[0-9]{6}$");
+// UC 4 Allow space after 3 digits along with the previous one
+let pincodeRegex = RegExp("^[0-9]{3}[ ]{0,1}[0-9]{3}$");
 let pincode;
 
 function validatePincode(input)
